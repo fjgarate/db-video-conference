be6f4c8a553f9d9279217c2af1a7fbf9274736b7
fjgarate <fgarate.dev@gmail.com>
2019-09-13 12:03:41 +0200

    control de sesiones 5

--- _helpers/db.js
+++ _helpers/db.js
@@ -6,5 +6,6 @@ mongoose.Promise = global.Promise;
 module.exports = {
     User: require('../users/user.model'),
     Conversation: require('../conversations/conversation.model'),
-    Appointment: require('../appointments/appointment.model')
+    Appointment: require('../appointments/appointment.model'),
+    Session: require('../sessions/session.model')
 };
